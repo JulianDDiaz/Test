1bf0bdd8c19c78916d8f833d632ab37a998c8392
Julian Diaz <juddiazci@unal.edu.co>
2023-11-21 13:01:40 -0500

    test

--- test.js
+++ test.js
@@ -1,1 +1,8 @@
-console.log('Hello World!');
+const fs = require('fs');
+
+fs.writeFile("./test_file.txt", "Hey there2!", function(err) {
+    if(err) {
+        return console.log(err);
+    }
+    console.log("The file was saved!");
+});
